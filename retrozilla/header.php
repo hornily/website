205989cc6e7c2d27bf0d2ceec82b506f37ca11a6
hornily <82062042+hornily@users.noreply.github.com>
2021-06-26 17:19:34 -0500

    Create header.php

---
+++ retrozilla/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>RetroZilla</title>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+<meta name="title" content="RetroZilla's Homepage (fake)">
+<meta name="description" content="Welcome!">
+<meta name="theme-color" content="#B103FF">
+</head>
+<body>
+<img src="img/header.png" usemap="#image-map">
+
+<map name="image-map">
+    <area target="" alt="Download" title="Download" href="http://rn10950.github.io/RetroZillaWeb/" coords="5,1,54,29" shape="rect">
+    <area target="" alt="About" title="About" href="about.php" coords="61,28,86,0,72,12" shape="rect">
+    <area target="" alt="pixel's page" title="pixel's page" href="http://pixel69.xyz" coords="503,0,529,27" shape="rect">
+    <area target="" alt="Aim" title="Aim" href="http://iwarg.ddns.net/phoenix/index.php" coords="534,2,574,23" shape="rect">
+</map>
+<br>
