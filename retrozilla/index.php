c90748052e5560e3aeab9a7b0a816b1755cfe993
hornily <82062042+hornily@users.noreply.github.com>
2021-06-26 17:19:50 -0500

    Add files via upload

---
+++ retrozilla/index.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<p>|</p>
+<font face="Arial">
+<h1>Welcome!</h2>
+<p>|</p>
+<h3>Welcome to the RetroZilla Homepage (fake). This was made by <a href="http://pixel69.xyz">pixel</a> for fun.<br> If you want to download retrozilla, click <a href="http://rn10950.github.io/RetroZillaWeb/">here!</a>
+<hr>
+<h1>Images</h1>
+<p>|</p>
+<img src="img/1.gif">
+<br>
+<img src="img/2.gif">
+<hr>
+<p>The end, &copy 1942-9999 gaming incorpaorated</p>
+</font>
